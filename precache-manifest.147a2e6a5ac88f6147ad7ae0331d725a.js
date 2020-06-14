9922e631205f6f7b341f41e1972c49bfb83693b1
Your Name <you@example.com>
2020-06-14 22:00:45 +0530

    Updates

---
+++ precache-manifest.147a2e6a5ac88f6147ad7ae0331d725a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc284a6182ceffa4254656262191ca7",
+    "url": "/banking-app/index.html"
+  },
+  {
+    "revision": "d59013f0058c9fa9828a",
+    "url": "/banking-app/static/js/2.31eb6a09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/banking-app/static/js/2.31eb6a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69423659a0be92175eab",
+    "url": "/banking-app/static/js/main.a1d31c7e.chunk.js"
+  },
+  {
+    "revision": "d6d394b0e4a7d5e58674",
+    "url": "/banking-app/static/js/runtime-main.b5c77d0c.js"
+  }
+]);
